47fdb704b1fb8e032ba00ee6cafdd4614f9ab5b1
MomoKiller <1226421458@qq.com>
2020-04-18 15:56:09 +0800

    提交一版可用

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -1,11 +1,22 @@
 <template>
   <div class="home">
-      <div>{{msg}}</div>
+    <div class="content-wrap">
+      <a class="music"></a>
+      <div class="theme"></div>
+      <div class="ink"></div>
+      <a class="enter"></a>
     </div>
+    <div class="footer">
+      <p>
+        <a>常熟市委宣传部出品</a>
+      </p>
+      <p>苏州巨数技术支持</p>
+    </div>
+  </div>
 </template>
 
 <script>
-import home from './home.js';
+import home from "./home.js";
 export default home;
 </script>
 
