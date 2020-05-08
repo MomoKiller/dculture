57f1ec8da71c09f4853932c691b6d83e5b82302c
MomoKiller <1226421458@qq.com>
2020-05-08 21:35:08 +0800

    提交分享提示

---
+++ src/components/share/Share.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="share">
+
+    </div>
+</template>
+
+<script>
+import share from "./share.js";
+export default share;
+</script>
+
+<style scoped>
+@import url(./share.css);
+</style>
