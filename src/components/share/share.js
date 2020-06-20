3bf6d547272a02a8f94839aafaedc281d037bdc7
MomoKiller <1226421458@qq.com>
2020-06-20 15:31:04 +0800

    稍作修改

--- src/components/share/share.js
+++ src/components/share/share.js
@@ -5,7 +5,13 @@ export default {
     data() {
         return {
             shareState: false,
-            timeOutEvent: 0   // 定时器
+            timeOutEvent: 0, // 定时器
+            works: {
+                name: '富春山居图',
+                author: '黄公望',
+                img: require('../../assets/img/share/share-img-default.png'),
+                ercode: require('../../assets/img/share/share-img-ercode.png')
+            }
         };
     },
     mounted() {
@@ -15,37 +21,37 @@ export default {
         // 生成海报
         toPost() {
             let self = this;
-            let DomeW = self.$refs.postref.offsetWidth;//获取目标元素的宽高
-            let DemoH = self.$refs.postref.offsetHeight;//获取目标元素的宽高
-            html2canvas(self.$refs.postref, { useCORS: true, width: DomeW, height: DemoH }).then(function (canvas) {
+            let DomeW = self.$refs.postref.offsetWidth; //获取目标元素的宽高
+            let DemoH = self.$refs.postref.offsetHeight; //获取目标元素的宽高
+            html2canvas(self.$refs.postref, { useCORS: true, width: DomeW, height: DemoH }).then(function(canvas) {
                 let imgUri = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream"); // 获取生成的图片的url
                 self.downloadIamge(imgUri, '富春山居图');
             })
         },
         gotouchstart() {
             let self = this;
-            clearTimeout(this.timeOutEvent);//清除定时器
+            clearTimeout(this.timeOutEvent); //清除定时器
             this.timeOutEvent = 0;
             /* 长按触发事件 */
-            this.timeOutEvent = setTimeout(function () {
+            this.timeOutEvent = setTimeout(function() {
                 self.toPost();
             }, 600);
         },
         /* 移动触发事件 */
         gotouchmove() {
             clearTimeout(this.timeOutEvent);
-            if (this.timeOutEvent != 0) { }
+            if (this.timeOutEvent != 0) {}
         },
         gotouchend() {
-            clearTimeout(this.timeOutEvent);//清除定时器
+            clearTimeout(this.timeOutEvent); //清除定时器
             this.timeOutEvent = 0;
         },
         /* 图片下载 */
-        downloadIamge(imgsrc, name) {//下载图片地址和图片名
+        downloadIamge(imgsrc, name) { //下载图片地址和图片名
             var image = new Image();
             // 解决跨域 Canvas 污染问题
             image.setAttribute("crossOrigin", "anonymous");
-            image.onload = function () {
+            image.onload = function() {
                 var canvas = document.createElement("canvas");
                 canvas.width = image.width;
                 canvas.height = image.height;
