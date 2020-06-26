3c4822ea191efae0e264b160e3732dcbd24ce986
MomoKiller <1226421458@qq.com>
2020-06-26 17:33:00 +0800

    提交

--- src/components/danceList/list.js
+++ src/components/danceList/list.js
@@ -64,9 +64,9 @@ export default {
         // 点击搜索按钮
         searchList() {
             // 重置条件
-            // this.seachParams.videotype = 0;
-            // this.seachParams.currentPage = 1;
-            // this.seachParams.pageSize = 5;
+            this.seachParams.videotype = 0;
+            this.seachParams.currentPage = 1;
+            this.seachParams.pageSize = 5;
             this.listObj = [];
             this.getListData();
         },
